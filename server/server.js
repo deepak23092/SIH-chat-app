f61c5d00a9a7194def534c5b1bafd56a451188a1
deepak23092 <deepaklohar974@gmail.com>
2024-12-12 02:31:16 +0530

    use all deployed site

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://sih-chat-app-frntnd.vercel.app",
     methods: ["GET", "POST"],
   },
 });
@@ -20,5 +20,5 @@ const io = new Server(server, {
 require("./src/socket")(io);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on https://sih-chat-app-frntnd.vercel.app/`);
 });
